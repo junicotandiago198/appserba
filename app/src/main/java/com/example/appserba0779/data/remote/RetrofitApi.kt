76823fba4c67ba76b11e01f514f80fb4daaa072a
junicotandiago198 <junicotandiagoo21@gmail.com>
2020-12-30 15:56:36 +0700

    AppApi

--- app/src/main/java/com/example/appserba0779/data/remote/RetrofitApi.kt
+++ app/src/main/java/com/example/appserba0779/data/remote/RetrofitApi.kt
@@ -7,7 +7,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
 object RetrofitApi {
-    const val NEWS_URL = "https://www.news.developeridn.com/"
+    const val NEWS_URL = "https://berita-news.herokuapp.com/"
 
     private val client = OkHttpClient.Builder()
         .addInterceptor(HttpLoggingInterceptor().apply {
